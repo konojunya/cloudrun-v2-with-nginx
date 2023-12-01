d42674c7b2835dfdfdc19963f1e63bd9c44c4ef4
konojunya <junya03dance@gmail.com>
2023-12-01 23:58:01 +0900

    fix: port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import {serve} from '@hono/node-server';
 import {Hono} from 'hono';
 
+const PORT = Number(process.env.PORT) || 8080;
+
 const app = new Hono();
 app.get('/', c => c.text('Hello Hono!'));
 
-serve(app);
+serve(
+  {
+    fetch: app.fetch,
+    port: PORT,
+  },
+  info => {
+    console.log(`Listening on http://localhost:${info.port}`);
+  }
+);
